3e3ded2c88c218995e3167651da6b8ebf147aaf3
FUJIKURA, Tsuyoshi <fujikura@media-groove.co.jp>
2025-05-21 15:44:40 +0900

    chore: eslint, prettier導入＆コードフォーマット

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "comma-dangle'": [2, 'always-multiline'],
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  prettierConfig,
+);
