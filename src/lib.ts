c5de19568f9d0ec1a8c70654ca6da636a69e50bb
FUJIKURA, Tsuyoshi <fujikura@media-groove.co.jp>
2025-05-28 18:47:39 +0900

    chore: ライブラリビルド設定

---
+++ src/lib.ts
@@ -0,0 +1,2 @@
+import './style.css';
+export { default as GoogleDocumentView } from './components/GoogleDocumentView.vue';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({ exclude: ['src/main.ts'], tsconfigPath: './tsconfig.app.json' })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib.ts'),
+      name: 'GoogleDocumentView Vue Component',
+      fileName: 'google-document-view',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
