cccb7fd04f453cf51827944607ca14e318050ad9
FUJIKURA, Tsuyoshi <fujikura@media-groove.co.jp>
2025-06-08 14:10:15 +0900

    chore: ビルド調整

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({ exclude: ['src/main.ts'], tsconfigPath: './tsconfig.app.json' })],
+  plugins: [vue(), dts({ exclude: ['src/main.ts'], tsconfigPath: './tsconfig.app.json' }), libInjectCss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
